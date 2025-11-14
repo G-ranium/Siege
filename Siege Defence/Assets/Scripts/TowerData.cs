48ac41cff5c8ca415a7870056c146d60dfb48547
AJ <11019728@uvu.edu>
2025-11-14 13:05:30 -0700

    Fixed Tower Behavior

--- Siege Defence/Assets/Scripts/TowerData.cs
+++ Siege Defence/Assets/Scripts/TowerData.cs
@@ -6,7 +6,11 @@ public class TowerData : ScriptableObject
     public string towerName;
     public int level;
     public float health;
+    public float maxHealth;
     public float fireRate;
     public int damage;
     public float range;
+    
+    [Header("Targeting")]
+    public LayerMask enemyLayer;
 }
